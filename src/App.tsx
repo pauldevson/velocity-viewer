640f842290cef2b9171f97f6646bf47e43d7213b
psanche3 <Paul.Sanchez@imail2.org>
2021-09-20 15:32:41 -0500

    first working attempty at rendering velocity templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
+import {
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  TextField,
+} from '@material-ui/core';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import React, { useState } from 'react';
+import { render } from 'velocityjs';
 import './App.css';
 
 function App() {
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          type: 'dark',
+        },
+      }),
+    []
+  );
+
+  const [template, setTemplate] = useState('');
+  const [model, setModel] = useState('');
+  const [html, setHtml] = useState('');
+
+  const handleTemplateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTemplate(event.target.value);
+  };
+  const handleModelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setModel(event.target.value);
+  };
+
+  const handlePreview = () => {
+    const modelObj = JSON.parse(model);
+    console.log(modelObj);
+    const rendered = render(template, modelObj);
+    setHtml(rendered);
+  };
+
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <div className="App">
+        <main className="App-body">
+          <Container maxWidth="lg">
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <p>Enter the velocity template and JSON model and hit preview.</p>
+            <Grid container justifyContent="center" spacing={3}>
+              <Grid item xs={7}>
+                <Card elevation={1}>
+                  <CardContent>
+                    <TextField
+                      id="filled-multiline-flexible"
+                      label="Velocity template"
+                      multiline
+                      maxRows={15}
+                      rows={15}
+                      value={template}
+                      onChange={handleTemplateChange}
+                      fullWidth
+                    />
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={5}>
+                <Card elevation={1}>
+                  <CardContent>
+                    <TextField
+                      id="filled-multiline-flexible"
+                      label="Velocity Context"
+                      multiline
+                      maxRows={15}
+                      rows={15}
+                      value={model}
+                      onChange={handleModelChange}
+                      fullWidth
+                    />
+                  </CardContent>
+                </Card>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  onClick={handlePreview}
+                >
+                  View Html
+                </Button>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Card elevation={1}>
+                  <CardContent>
+                    <TextField
+                      id="filled-multiline-flexible"
+                      label="Rendered HTML"
+                      multiline
+                      maxRows={30}
+                      rows={30}
+                      value={html}
+                      fullWidth
+                    />
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </Container>
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
 
